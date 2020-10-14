8b41599e02379f868a16a06233e5d6fd4f2a51ae
vianziro <vianziro@gmail.com>
2020-10-14 13:36:38 +0700

    ujicoba buat aplikasi

---
+++ fundamental/main.py
@@ -0,0 +1,12 @@
+import requests
+
+print("testing")
+
+try:
+    r = requests.get('https://google.com')
+    print(r.status_code)
+
+    if r.status_code == 200:
+        print(r.text)
+except Exception as e:
+    print('ada error' , e)
