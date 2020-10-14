c2c16ae9948053c7285d1735198669f0d0ff1737
vianziro <vianziro@gmail.com>
2020-10-14 23:53:15 +0700

    ujicoba buat aplikasi

--- fundamental/main.py
+++ fundamental/main.py
@@ -1,12 +1,17 @@
-import requests
+# import requests
+#
+# print("testing")
+#
+# try:
+#     r = requests.get('https://google.com')
+#     print(r.status_code)
+#
+#     if r.status_code == 200:
+#         print(r.text)
+# except Exception as e:
+#     print('ada error' , e)
 
-print("testing")
 
-try:
-    r = requests.get('https://google.com')
-    print(r.status_code)
-
-    if r.status_code == 200:
-        print(r.text)
-except Exception as e:
-    print('ada error' , e)
+print("hello word")
+print("selamat datang dibelajar tahap awal")
+print("-" * 10)
