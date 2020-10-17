9cbc98925ada8da6c2b1f1b2d56c97ac89affb75
vianziro <vianziro@gmail.com>
2020-10-17 23:14:24 +0700

    lanjutan belajar python

---
+++ pakinggae_modular/coba.py
@@ -0,0 +1,3 @@
+from hitung.segitiga import hitung_luas_segitiga
+
+print(f'hitung nilai keliling segita {hitung_luas_segitiga(10,2)}')
---
+++ part2/fungsi.py
@@ -0,0 +1,11 @@
+alas = 10
+tinggi = 6
+# luas_segitiga = alas * tinggi/2
+# print(f'Segitiga dengan alas = {alas} dan tinggi = {tinggi} memiliki luas {luas_segitiga}')
+#
+
+def hitung_segitiga(alas, tinggi) :
+    return  alas * tinggi/2
+
+
+print(hitung_segitiga(100,20))
